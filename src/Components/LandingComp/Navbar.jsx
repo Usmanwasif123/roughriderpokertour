3fab0517aa00491395523ce01a200f3f2a61179b
Usmanwasif123 <usmanwasif8@gmail.com>
2024-06-28 23:56:04 +0500

    manage account div made functionality left .

--- src/Components/LandingComp/Navbar.jsx
+++ src/Components/LandingComp/Navbar.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 
 function Navbar() {
+  
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [showContent, setShowContent] = useState(false);
+
+  const toggleContent = (e) => {
+    e.preventDefault(); // Prevents the default behavior of refreshing the page
+    setShowContent(!showContent);
+};
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
@@ -60,13 +67,43 @@ function Navbar() {
             </li>
           </ul>
         </div>
-        <div>
-          <a href='/edit-your-profile'><img className='cursor-pointer'
-           src='https://i.ibb.co/cXwcdnh/Group.png' alt='user' />
+        <div className='relative inline-block'>
+          <a href='/' onClick={toggleContent} className='cursor-pointer'>
+          <img src='https://i.ibb.co/cXwcdnh/Group.png' alt='user' />
            </a>
+           {showContent && (
+                <div className="absolute -left-40 bg-[#F3D284] w-5/3 shadow-lg px-1.5 py-5 mt-1.5 rounded-lg text-sm">
+                <div className="font-bold flex gap-1 flex-col items-center justify-center">
+                    <div className='flex flex-row items-center'>
+                    <div className='flex flex-row gap-2 rounded-full py-1 items-center'>
+                        <img src='https://i.ibb.co/f4hyV98/user.png' className='h-6 w-6 ' alt='user icon'/>
+                    </div>
+                    <div>
+                    <h2 className="text-black text-md">Manage My Account</h2>
+                    </div>
+                    </div>
+                    <div className='flex flex-row items-center'>
+                    <div className='flex gap-5 rounded-full px-5 py-2 items-center -ml-12'>
+                        <img src='https://i.ibb.co/QMxC359/Vector.png' className='h-6 w-6' alt='user icon'/>
+                    </div>
+                    <div>
+                      <h2 className="text-black text-md">My Reviews</h2>
+                    </div>
+                    </div>
+                    <div className='flex flex-row items-center'>
+                    <div className='flex flex-row gap-5 rounded-full px-5 py-2 items-center -ml-20'>
+                        <img src='https://i.ibb.co/L5s4Ch4/Icon-logout.png' className='h-6 w-6' alt='user icon'/>
+                    </div>
+                    <div>
+                    <h2 className="text-black text-md">Logout</h2>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            )}
         </div>
         <div>
-          <img src='https://i.ibb.co/ctSxybB/Group-5108.png' alt='user' />
+          <img src='https://i.ibb.co/ctSxybB/Group-5108.png' alt='notification' />
         </div>
       </div>
     </nav>
